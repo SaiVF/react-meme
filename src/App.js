f9594421c55df1fe686f879b500306450fcb5c6e
Santiago Vargas <santiago.vargas@itti.digital>
2023-03-06 20:05:27 -0300

    Creacion de proyecto

---
+++ src/App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <select>
+        <option value="fire"> Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select>{" "}
+      <br />
+      <input type="text" placeholder="Linea 1"/>
+      <br />
+      <input type="text" placeholder="Linea 2"/>
+      <br />
+      <button>Exportar</button>
+
+      <div>
+        <span>Linea 1</span>
+        <span>Linea 2</span>
+        <img src="" />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
